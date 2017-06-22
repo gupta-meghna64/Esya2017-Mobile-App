be1d5ba1104feeb74fb9b6a8ec991e836e0b6dcf
Meghna Gupta <meghnagupta64@gmail.com>
2017-06-22 19:15:28 +0530

    First section added

--- DiscrollView/app/src/main/java/com/example/discrollview/MainActivity.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.discrollview;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView) findViewById(R.id.starterImage);
+        img.setBackgroundResource(R.drawable.progress_animation);
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+        frameAnimation.start();
     }
 }
