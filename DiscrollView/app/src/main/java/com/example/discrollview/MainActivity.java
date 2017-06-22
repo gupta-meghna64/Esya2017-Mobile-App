bc20ba3c14010eb597fa5f0af06269e5587c7e22
saatvikj <saatvik16261@iiitd.ac.in>
2017-06-22 19:58:08 +0530

    Image buttons made clickable
    Changed a few transitions as well.

--- DiscrollView/app/src/main/java/com/example/discrollview/MainActivity.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.discrollview;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,61 @@ public class MainActivity extends AppCompatActivity {
         img.setBackgroundResource(R.drawable.progress_animation);
         AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
         frameAnimation.start();
+
+        ImageView cat1 = (ImageView) findViewById(R.id.iconCat1);
+        ImageView cat2 = (ImageView) findViewById(R.id.iconCat2);
+        ImageView cat3 = (ImageView) findViewById(R.id.iconCat3);
+        ImageView cat4 = (ImageView) findViewById(R.id.iconCat4);
+        ImageView cat5 = (ImageView) findViewById(R.id.iconCat5);
+        ImageView cat6 = (ImageView) findViewById(R.id.iconCat6);
+
+        cat1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"I will redirect to 1", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        cat1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"I will redirect to 1", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        cat2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"I will redirect to 2", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        cat3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"I will redirect to 3", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        cat4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"I will redirect to 4", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        cat5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"I will redirect to 5", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        cat6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"I will redirect to 6", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
