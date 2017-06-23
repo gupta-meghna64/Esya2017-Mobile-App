58ac959b639a94d8f7dd86df01c5c782eed9584e
Meghna Gupta <meghnagupta64@gmail.com>
2017-06-23 19:52:31 +0530

    Home page Functionalities added

--- DiscrollView/app/src/main/java/com/example/discrollview/MainActivity.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.discrollview;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +21,62 @@ public class MainActivity extends AppCompatActivity {
         img.setBackgroundResource(R.drawable.progress_animation);
         AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
         frameAnimation.start();
+
+        ImageButton facebook = (ImageButton) findViewById(R.id.facebookEsya);
+        ImageButton instagram = (ImageButton) findViewById(R.id.instagramEsya);
+        ImageButton twitter = (ImageButton) findViewById(R.id.twitterEsya);
+        ImageButton website = (ImageButton) findViewById(R.id.websiteEsya);
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Uri uri = Uri.parse("https://www.facebook.com/EsyaIIITD/"); // missing 'http://' will cause crashed
+//                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//                startActivity(intent);
+//                getOpenFacebookIntent(getApplicationContext());
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://profile/Fe9qooYPLQd"));
+                    startActivity(intent);
+                } catch(Exception e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/EsyaIIITD")));
+                }
+            }
+        });
+
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://instagram.com/_u/esya_iiitd");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://instagram.com/esya_iiitd")));
+                }
+            }
+        });
+
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?screen_name=" + "esyaiiitd")));
+                }catch (Exception e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/#!/" + "esyaiiitd")));
+                }
+            }
+        });
+
+        website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://esya.iiitd.edu.in/"));
+                startActivity(browserIntent);
+            }
+        });
+
     }
 }
