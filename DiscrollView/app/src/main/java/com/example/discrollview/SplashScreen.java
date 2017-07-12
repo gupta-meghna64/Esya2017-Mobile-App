b28d0db84c1f41a92284093f0dcd0aa9577b4198
Meghna Gupta <meghnagupta64@gmail.com>
2017-07-12 22:05:13 +0530

    Splash Screen Added

--- DiscrollView/app/src/main/java/com/example/discrollview/SplashScreen.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/SplashScreen.java
@@ -3,6 +3,7 @@ package com.example.discrollview;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
@@ -28,26 +29,61 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        startHeavyProcessing();
         AnimatedSvgView svgView = (AnimatedSvgView) findViewById(R.id.animated_svg_view);
         svgView.start();
-        FadingTextView changingText=(FadingTextView) findViewById(R.id.fadingTextView);
-        Typeface tf = Typeface.createFromAsset(getAssets(),"Roboto-Light.ttf");
-        changingText.setTypeface(tf);
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+//        FadingTextView changingText=(FadingTextView) findViewById(R.id.fadingTextView);
+//        Typeface tf = Typeface.createFromAsset(getAssets(),"Roboto-Light.ttf");
+//        changingText.setTypeface(tf);
+//        Timer timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//
+//            public void run() {
+//
+//                //here you can start your Activity B.
+//                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+//                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                startActivity(i);
+//
+//            }
+//
+//        }, 5000);
 
-            public void run() {
+    }
 
-                //here you can start your Activity B.
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(i);
+    private void startHeavyProcessing(){
+        new LongOperation().execute("");
+    }
 
+    private class LongOperation extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            //some heavy processing resulting in a Data String
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(900);
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                }
             }
+            return "whatever result you have";
+        }
 
-        }, 5000);
+        @Override
+        protected void onPostExecute(String result) {
+            Intent i = new Intent(SplashScreen.this, MainActivity.class);
+            i.putExtra("data", result);
+            startActivity(i);
+            finish();
+        }
 
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
     }
 
 }
