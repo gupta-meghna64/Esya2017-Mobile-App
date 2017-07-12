478b9ba42ab287e8701ae1d5d2d38df22a53d67b
Saatvik Jain <saatvik16261@iiitd.ac.in>
2017-07-12 15:24:29 +0530

    Logo path splash screen testing

--- DiscrollView/app/src/main/java/com/example/discrollview/SplashScreen.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/SplashScreen.java
@@ -17,15 +17,23 @@ public class SplashScreen extends AwesomeSplash {
 
 			/* you don't have to override every property */
 
-        configSplash.setBackgroundColor(R.color.black);
-        configSplash.setLogoSplash(R.mipmap.esyalogo);
-        configSplash.setAnimLogoSplashDuration(2000);
-        configSplash.setAnimLogoSplashTechnique(Techniques.Landing);
+        configSplash.setBackgroundColor(R.color.white);
+        configSplash.setAnimCircularRevealDuration(50);
+        configSplash.setPathSplash("M 135.44554,259.48515 191.52475,259.9604 274.21782,344.07921 131.64356,344.55446 45.623762,257.58416 V 129.26733 L 132.11881,42.772277 257.58416,42.29703 342.65347,129.74257 342.17822,194.85149 264.71287,271.36634 187.24752,192.9505 253.78218,193.90099 254.25743,127.36634 135.44554,128.31683 Z"); //set path String
+        configSplash.setOriginalHeight(384); //in relation to your svg (path) resource
+        configSplash.setOriginalWidth(389); //in relation to your svg (path) resource
+        configSplash.setAnimPathStrokeDrawingDuration(3000);
+        configSplash.setPathSplashStrokeSize(4); //I advise value be <5
+        configSplash.setPathSplashStrokeColor(R.color.black); //any color you want form colors.xml
+        configSplash.setAnimPathFillingDuration(3000);
+        configSplash.setPathSplashFillColor(R.color.esyaLogoColor); //path object filling color
         configSplash.setTitleSplash("Esya'17");
-        configSplash.setTitleTextColor(R.color.white);
+        configSplash.setTitleTextColor(R.color.black);
         configSplash.setTitleTextSize(30f); //float value
         configSplash.setAnimTitleDuration(3000);
-        configSplash.setAnimTitleTechnique(Techniques.Landing);
+        configSplash.setAnimTitleTechnique(Techniques.FadeIn);
+
+
 
     }
 
