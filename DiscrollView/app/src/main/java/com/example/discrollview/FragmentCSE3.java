8aae783882b32010690db0889b82937c7aa45dcf
Saatvik Jain <saatvik16261@iiitd.ac.in>
2017-07-14 02:10:27 +0530

    App crash issue solved.

--- DiscrollView/app/src/main/java/com/example/discrollview/FragmentCSE3.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/FragmentCSE3.java
@@ -1,18 +1,39 @@
 package com.example.discrollview;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by HP on 01-07-2017.
  */
 
 public class FragmentCSE3 extends Fragment {
+
+    private Button register;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_cse3, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        register=(Button) getActivity().findViewById(R.id.buttonRegisterPJ);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://docs.google.com/forms/d/e/1FAIpQLScnRTjz5mE4jypXB4T2rokaslZwSk9ckKfpc1s4FH_1VgLKow/viewform")));
+            }
+        });
+
     }
 }
