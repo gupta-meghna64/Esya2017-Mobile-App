aaeb44eabfd03c13a9b2dae5ba17937ae7c36421
Meghna Gupta <meghnagupta64@gmail.com>
2017-07-15 20:24:52 +0530

    ECE images rectified

--- DiscrollView/app/src/main/java/com/example/discrollview/TabTwoFragment.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/TabTwoFragment.java
@@ -1,12 +1,14 @@
 package com.example.discrollview;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by HP on 01-07-2017.
@@ -21,7 +23,7 @@ public class TabTwoFragment extends Fragment {
         CardView circuitrix = (CardView) v.findViewById(R.id.cardCircuitrix);
         CardView robowars = (CardView) v.findViewById(R.id.cardRobowars);
         CardView robocon = (CardView) v.findViewById(R.id.cardRobocon);
-    //    CardView robomaze = (CardView) v.findViewById(R.id.cardRobomaze);
+        //    CardView robomaze = (CardView) v.findViewById(R.id.cardRobomaze);
         CardView robosoccer = (CardView) v.findViewById(R.id.cardRobosoccer);
         CardView hackoverflow = (CardView) v.findViewById(R.id.cardHackoverflow);
 
@@ -91,4 +93,44 @@ public class TabTwoFragment extends Fragment {
 
         return v;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        int imageHeight = (int) Math.round(Resources.getSystem().getDisplayMetrics().widthPixels / 2.69);
+        int imageWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+
+        ImageView cjCover = (ImageView) getActivity().findViewById(R.id.circuitrixjrCover);
+        cjCover.getLayoutParams().width = imageWidth;
+        cjCover.getLayoutParams().height = imageHeight;
+        cjCover.requestLayout();
+
+        ImageView circuitrixCover = (ImageView) getActivity().findViewById(R.id.circuitrixCover);
+        circuitrixCover.getLayoutParams().width = imageWidth;
+        circuitrixCover.getLayoutParams().height = imageHeight;
+        circuitrixCover.requestLayout();
+
+        ImageView robowarsCover = (ImageView) getActivity().findViewById(R.id.robowarsCover);
+        robowarsCover.getLayoutParams().width = imageWidth;
+        robowarsCover.getLayoutParams().height = imageHeight;
+        robowarsCover.requestLayout();
+
+        ImageView roboconCover = (ImageView) getActivity().findViewById(R.id.roboconCover);
+        roboconCover.getLayoutParams().width = imageWidth;
+        roboconCover.getLayoutParams().height = imageHeight;
+        roboconCover.requestLayout();
+
+        ImageView robosoccerCover = (ImageView) getActivity().findViewById(R.id.robosoccerCover);
+        robosoccerCover.getLayoutParams().width = imageWidth;
+        robosoccerCover.getLayoutParams().height = imageHeight;
+        robosoccerCover.requestLayout();
+
+        ImageView hackoverflowCover = (ImageView) getActivity().findViewById(R.id.hackoverflowCover);
+        hackoverflowCover.getLayoutParams().width = imageWidth;
+        hackoverflowCover.getLayoutParams().height = imageHeight;
+        hackoverflowCover.requestLayout();
+
+
+    }
+
 }
