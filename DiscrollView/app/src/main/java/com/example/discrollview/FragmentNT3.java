87f509d144606ea0b59263b2e05b9afa6d14e922
Saatvik Jain <saatvik16261@iiitd.ac.in>
2017-07-15 16:44:05 +0530

    Registration form added for Non tech events.

--- DiscrollView/app/src/main/java/com/example/discrollview/FragmentNT3.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/FragmentNT3.java
@@ -1,18 +1,37 @@
 package com.example.discrollview;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by HP on 01-07-2017.
  */
 
 public class FragmentNT3 extends Fragment {
+
+    private Button register;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_nt3, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        register=(Button) getActivity().findViewById(R.id.buttonRegisterQuiz);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/forms/d/e/1FAIpQLSfG6m_W6g8oN51PzffryoOIWED5Rupoa_nCCHt5G70lP3HIbQ/viewform")));
+            }
+        });
+
     }
 }
