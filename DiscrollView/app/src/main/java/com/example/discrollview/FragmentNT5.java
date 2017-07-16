2cae546886890f98cb884bf5bdc2da215e10ecb4
Saatvik Jain <saatvik16261@iiitd.ac.in>
2017-07-16 16:05:49 +0530

    PWNED rules added.

--- DiscrollView/app/src/main/java/com/example/discrollview/FragmentNT5.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/FragmentNT5.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by HP on 01-07-2017.
@@ -24,6 +25,15 @@ public class FragmentNT5 extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        TextView rules = (TextView) getActivity().findViewById(R.id.pwnedRulebook);
+        rules.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/document/d/1E8fbNAYFejPGDxcfFiBwmXdqJEZ535m9GMMgYyIB9bg/edit")));
+            }
+        });
+
 
         register=(Button) getActivity().findViewById(R.id.buttonRegisterPWNED);
         register.setOnClickListener(new View.OnClickListener() {
