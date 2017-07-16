55bfca70389ba9269d4c0d197b862cf0809a64c3
Saatvik Jain <saatvik16261@iiitd.ac.in>
2017-07-16 17:15:26 +0530

    16 words to go registration form added.

--- DiscrollView/app/src/main/java/com/example/discrollview/FragmentNT8.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/FragmentNT8.java
@@ -1,11 +1,14 @@
 package com.example.discrollview;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 /**
@@ -13,6 +16,8 @@ import android.widget.ImageView;
  */
 
 public class FragmentNT8 extends Fragment {
+    private Button register;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_nt8, container, false);
@@ -20,6 +25,14 @@ public class FragmentNT8 extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        register = (Button) getActivity().findViewById(R.id.buttonRegisterWordsToGo);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/a/iiitd.ac.in/forms/d/e/1FAIpQLScbjszH_V2C9wy0eioHtqB6tQynoao4QI5Rl6_Kd1AsasRYIQ/viewform")));
+            }
+        });
 
         int imageHeight = (int) Math.round(Resources.getSystem().getDisplayMetrics().widthPixels / 2.69);
         int imageWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
