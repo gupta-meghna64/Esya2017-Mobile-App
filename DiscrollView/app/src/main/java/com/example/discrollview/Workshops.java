2e63906f87a2d16accf16838dd58d877b1a2538e
Meghna Gupta <meghnagupta64@gmail.com>
2017-08-14 14:25:30 +0530

    Workshop page updated

--- DiscrollView/app/src/main/java/com/example/discrollview/Workshops.java
+++ DiscrollView/app/src/main/java/com/example/discrollview/Workshops.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -26,27 +27,41 @@ public class Workshops extends AppCompatActivity {
     private Button back;
     private LinearLayout mainLayout;
     private ImageView canon;
+    private Button registerInfizeal;
+    private Button registerCanon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.workshops_main);
+        registerCanon = (Button) findViewById(R.id.applyCanon);
+        registerInfizeal = (Button) findViewById(R.id.applyInfizeal);
         canon = (ImageView) findViewById(R.id.canonImageView);
-        canon.getLayoutParams().height = Resources.getSystem().getDisplayMetrics().widthPixels;
-        canon.getLayoutParams().width = Resources.getSystem().getDisplayMetrics().widthPixels;
-        canon.requestLayout();
-        back.setOnClickListener(new View.OnClickListener() {
+//        canon.getLayoutParams().height = Resources.getSystem().getDisplayMetrics().widthPixels;
+//        canon.getLayoutParams().width = Resources.getSystem().getDisplayMetrics().widthPixels;
+//        canon.requestLayout();
+//        back.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent homeIntent = new Intent(Workshops.this, MainActivity.class);
+//                startActivity(homeIntent);
+//            }
+//        });
+
+        registerInfizeal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent homeIntent = new Intent(Workshops.this, MainActivity.class);
-                startActivity(homeIntent);
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/a/iiitd.ac.in/forms/d/e/1FAIpQLSfoEaKC2FEVrF-SSz8IVhDwKkQtyVTC7Zvik7FNNCCre6Ofgw/viewform")));
             }
         });
 
-        Bitmap workshopsBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mainworkshops);
-        Bitmap newResizedBMP = Bitmap.createScaledBitmap(workshopsBitmap, 1280, 1280, true);
-        Bitmap finalBMP = Bitmap.createBitmap(newResizedBMP, 250, 0, 768, 1280);
-        mainLayout.setBackground(new BitmapDrawable(finalBMP));
+        registerCanon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/forms/d/150ghm70SYdQKSCn_HjGh3cq4lfhI1Twi8dJHqGT0JEs/viewform?edit_requested=true")));
+            }
+        });
+
     }
 
     @Override
